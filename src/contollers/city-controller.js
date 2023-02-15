2883ca1bcf22e717f1a13e9ffbb75ef4add60d78
Chinmayee Ahire <theroadtotae@gmail.com>
2023-02-15 13:30:53 +0530

    Added city controller functions

---
+++ src/contollers/city-controller.js
@@ -0,0 +1,90 @@
+const { CityService}=require('../services/index');
+
+// post
+// data->req.body
+const cityService=new CityService();
+
+const create = async (req,res)=>{
+   try {
+    const city=await cityService.createCity(req.body);
+    return res.status(201).json({
+        data: city,
+        success: true,
+        message: 'Successfully created a city',
+        err: {}
+    });
+   } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+        data: {},
+        success: false,
+        message: 'Not able to crete a city',
+        err: error
+    });
+   }
+}
+//DELETE. -> /city/:id
+const destroy = async (req,res)=>{
+    try {
+        const response=await cityService.deleteCity(req.params.id);
+        return res.status(201).json({
+            data: response,
+            success: true,
+            message: 'Successfully deleted a city',
+            err: {}
+        });
+    } catch (error) {
+     console.log(error);
+     return res.status(500).json({
+        data: {},
+        success: false,
+        message: 'Not able to delete a city',
+        err: error
+    });
+    }
+}
+//GET-> /city/:id
+const get=async (req,res)=>{
+    try {
+        const response=await cityService.getCity(req.params.id);
+        return res.status(500).json({
+            data: response,
+            success: true,
+            message: 'successfully fetched a city',
+            err: {}
+        });
+    } catch (error) {
+     console.log(error);
+     return res.status(500).json({
+        data: {},
+        success: false,
+        message: 'Not able to get a city',
+        err: error
+    });
+    }
+}
+
+//PATCH-> /city/:id -> req.body
+const update=async (req,res)=>{
+    try {
+        const response=await cityService.updateCity(req.params.id, req.body);
+        return res.status(500).json({
+            data: response,
+            success: true,
+            message: 'successfully updated a city',
+            err: {}
+        });
+    } catch (error) {
+     console.log(error);
+     return res.status(500).json({
+        data: {},
+        success: false,
+        message: 'Not able to update a city',
+        err: error
+    });
+    }
+}
+
+module.exports={
+    create,destroy,get,update
+}
